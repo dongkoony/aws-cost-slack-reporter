fe6bd7a98a390fabb3e89713f7a4241c831e7a32
Dong Hyeon, Shin <109497684+dongkoony@users.noreply.github.com>
2025-07-16 21:46:14 +0900

    refactor: __init__.py에서 모듈 및 함수 임포트 방식을 개선하여 가독성 향상

--- src/__init__.py
+++ src/__init__.py
@@ -7,35 +7,29 @@ __version__ = "0.1.0"
 __author__ = "AWS Cost Slack Reporter Team"
 
 # 주요 모듈들
-from . import holiday_checker
-from . import cost_explorer
-from . import exchange_rate
-from . import chart_generator
-from . import slack_utils
-from . import lambda_function
-
+from . import (chart_generator, cost_explorer, exchange_rate, holiday_checker,
+               lambda_function, slack_utils)
+from .cost_explorer import get_cost_summary
+from .exchange_rate import convert_usd_to_krw, get_exchange_rate
+from .holiday_checker import should_send_report
 # 주요 함수들
 from .lambda_function import lambda_handler
-from .holiday_checker import should_send_report
-from .cost_explorer import get_cost_summary
-from .exchange_rate import get_exchange_rate, convert_usd_to_krw
 from .slack_utils import send_cost_report, test_slack_connection
 
 __all__ = [
     # 모듈
-    'holiday_checker',
-    'cost_explorer', 
-    'exchange_rate',
-    'chart_generator',
-    'slack_utils',
-    'lambda_function',
-    
+    "holiday_checker",
+    "cost_explorer",
+    "exchange_rate",
+    "chart_generator",
+    "slack_utils",
+    "lambda_function",
     # 주요 함수
-    'lambda_handler',
-    'should_send_report',
-    'get_cost_summary',
-    'get_exchange_rate',
-    'convert_usd_to_krw',
-    'send_cost_report',
-    'test_slack_connection'
+    "lambda_handler",
+    "should_send_report",
+    "get_cost_summary",
+    "get_exchange_rate",
+    "convert_usd_to_krw",
+    "send_cost_report",
+    "test_slack_connection",
 ]
